605d3557121e6a2f467525d06c01ef1a00530d21
kolyose <kolyose@gmail.com>
2016-10-05 17:53:36 +0300

    client: currentMilestoneVO  reset added

--- Assets/Scripts/view/GameBoard.cs
+++ Assets/Scripts/view/GameBoard.cs
@@ -185,6 +185,8 @@ public class GameBoard : MonoBehaviour, IGameBoard {
                         break;
                     }                 
             }
+            
+            currentMilestoneVO = null;
             return;
         }
 
